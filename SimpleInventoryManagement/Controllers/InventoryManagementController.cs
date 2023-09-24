b3638bbe480a18801e9fee910199caaa77f864a4
Diogo Pereira <diogo.machado.pereira@gmail.com>
2023-09-24 20:28:51 -0300

    Implementing CRUD

--- SimpleInventoryManagement/Controllers/InventoryManagementController.cs
+++ SimpleInventoryManagement/Controllers/InventoryManagementController.cs
@@ -21,6 +21,30 @@ namespace SimpleInventoryManagement.Controllers
             _logger = logger;
         }
 
+        [HttpPost]
+        public ActionResult Post([FromBody] ProductModel productModel)
+        {
+            var existingProducutItem = AllProducts.Find(p => p.Name == productModel.Name);
+            if (existingProducutItem != null)
+            {
+                return Conflict("Cannot create the product because it already exists.");
+            }
+            else
+            {
+                AllProducts.Add(productModel);
+                var resourceUrl = Request.Path.ToString() + '/' + productModel.Name;
+                return Created(resourceUrl, productModel);
+            }
+        }
+
+        [HttpGet]
+        [Route("{name}")]
+        public ActionResult<ProductModel> Get(string name)
+        {
+            var productItem = AllProducts.Find(p => p.Name == name);
+            return productItem == null ? NotFound() : Ok(productItem);
+        }
+
         [HttpGet]
         [Route("{sortKey},{ascending:bool}")]
         public ActionResult<List<ProductModel>> Get(string sortKey = "name", bool ascending = true)
@@ -45,6 +69,39 @@ namespace SimpleInventoryManagement.Controllers
            
             return Ok(products);
         }
-        
+
+        [HttpPut]
+        public ActionResult Update([FromBody] ProductModel productModel)
+        {
+            var existingProducutItem = AllProducts.Find(p => p.Name == productModel.Name);
+            if (existingProducutItem == null)
+            {
+                return BadRequest("Cannot update a nont existing product.");
+            }
+            else
+            {
+                existingProducutItem.Name = productModel.Name;
+                existingProducutItem.Price = productModel.Price;
+                existingProducutItem.Stock = productModel.Stock;
+                return Ok();
+            }
+        }
+
+        [HttpDelete]
+        [Route("{name}")]
+        public ActionResult Delete(string name)
+        {
+            var productItem = AllProducts.Find(x => x.Name == name);
+            if (productItem == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                AllProducts.Remove(productItem);
+                return NoContent();
+            }
+        }
+
     }
 }
